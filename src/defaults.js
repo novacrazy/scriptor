3e4d39a15b49d155822f9ab2aa14c97e5241bcff
novacrazy <novacrazy@gmail.com>
2015-07-05 02:34:12 -0500

    src: Added default values and AMD header value to defaults.js

---
+++ src/defaults.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Aaron on 7/4/2015.
+ */
+
+export var default_max_recursion = 9;
+
+export var default_max_debounceMaxWait = 50;
+
+export var AMD_Header = "if(typeof define !== 'function' && typeof module.define === 'function') {var define = module.define;}";
+
+export var default_dependencies = ['require', 'exports', 'module', 'imports'];
