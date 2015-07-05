54cf6b526548633627ef1de4d56871f1c17f0ae9
novacrazy <novacrazy@gmail.com>
2015-07-05 10:23:05 -0500

    Remove all old code

--- test/scripts/common.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by novacrazy on 2/3/2015.
- */
-
-var crypto = require( 'crypto' );
-
-exports.md5 = function(message) {
-    return crypto.createHash( 'md5' ).update( message, 'utf-8' ).digest( 'hex' );
-};
--- ts_src/typings/tsd.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="node/node.d.ts" />
-/// <reference path="bluebird/bluebird.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
