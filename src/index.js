70c3524f56044ec527eecee1054732e74df895e8
novacrazy <novacrazy@gmail.com>
2015-07-05 08:39:54 -0500

    src: Add index.js file to merge together components.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Aaron on 7/5/2015.
+ */
+
+/*
+ * This file uses ES7 export extensions
+ * */
+
+export
+Script
+from
+'./script';
+export
+Manager
+from
+'./manager';
+export
+Reference
+from
+'./reference';
