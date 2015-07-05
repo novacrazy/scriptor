152e40b640fe55b3d070413c2dd2dd1571e755a0
novacrazy <novacrazy@gmail.com>
2015-07-05 08:38:36 -0500

    Remove old async/sync top-level files.

--- sync.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by novacrazy on 6/17/14.
- */
-
-module.exports = require( './ts_build/sync.js' );
-
-module.exports.bin = function() {
-    return require( './bin/scriptor.js' ).apply( null, arguments );
-};
