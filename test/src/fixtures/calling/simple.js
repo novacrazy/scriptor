b0153f9a5bb193bf8f6e4b935905df12b9958f80
novacrazy <novacrazy@gmail.com>
2015-07-06 08:56:13 -0500

    test: Rearrange tests and test for calling exported functions.

---
+++ test/src/fixtures/calling/simple.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Aaron on 7/6/2015.
+ */
+
+module.exports = function() {
+    return {
+        towel: "Don't Forget Yours"
+    };
+};
---
+++ test/src/script_calling.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Aaron on 7/6/2015.
+ */
+
+import {runTests} from './runner.js';
+
+import assert from 'assert';
+
+import Promise from 'bluebird';
+
+var Module = require( 'module' );
+var path = require( 'path' );
+var fs = require( 'fs' );
+//Draws from the same node_modules folder, so they should be exact
+var touch = require( 'touch' );
+
+let tests = ( Scriptor, build ) => {
+    describe( `Script calling (${build} build)`, () => {
+        let Script = Scriptor.Script;
+
+        describe( 'empty file', function() {
+            let script = new Script( './test/build/fixtures/empty.js', module );
+
+            it( 'should not execute anything but just return the empty exports', function( done ) {
+                script.call().then( function( result ) {
+                    assert.deepEqual( result, {} );
+                    assert( script.loaded );
+
+                } ).then( done );
+            } );
+        } );
+
+        describe( 'simple module.exports = function(){...}', function() {
+            let script = new Script( './test/build/fixtures/calling/simple.js', module );
+
+            it( 'should call the exported function and return the result', function( done ) {
+                script.call().then( function( result ) {
+                    assert.deepEqual( result, {
+                        towel: "Don't Forget Yours"
+                    } );
+                    assert( script.loaded );
+
+                } ).then( done );
+            } );
+        } );
+    } );
+};
+
+runTests( 'compat', tests );
+runTests( 'modern', tests );
