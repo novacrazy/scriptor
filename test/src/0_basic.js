23f59901e310e5872ef7fd2dbaf27a0147f9ed15
novacrazy <novacrazy@gmail.com>
2015-07-07 05:48:41 -0500

    test: Add tests for the latest additional exports.

--- test/src/0_basic.js
+++ test/src/0_basic.js
@@ -14,6 +14,22 @@ let tests = ( Scriptor, build ) => {
             assert.strictEqual( typeof Scriptor.Script, 'function', 'No Script class exported' );
         } );
 
+        it( 'should have exported a SourceScript class', () => {
+            assert.strictEqual( typeof Scriptor.SourceScript, 'function', 'No SourceScript class exported' );
+        } );
+
+        it( 'should have exported a TextScript class', () => {
+            assert.strictEqual( typeof Scriptor.TextScript, 'function', 'No TextScript class exported' );
+        } );
+
+        it( 'should have exported a compile function', () => {
+            assert.strictEqual( typeof Scriptor.compile, 'function', 'No compile function exported' );
+        } );
+
+        it( 'should have exported a load function', () => {
+            assert.strictEqual( typeof Scriptor.load, 'function', 'No load function exported' );
+        } );
+
         it( 'should have exported a Manager class', () => {
             assert.strictEqual( typeof Scriptor.Manager, 'function', 'No Manager class exported' );
         } );
@@ -25,6 +41,10 @@ let tests = ( Scriptor, build ) => {
         it( 'should have exported a bluebird Promise class', () => {
             assert.strictEqual( typeof Scriptor.Promise, 'function', 'No Promise class exported' );
             assert.strictEqual( Scriptor.Promise, Promise, 'Exported Promise is not bluebird' );
+        } );
+
+        it( 'should have exported a addYieldHandler function', () => {
+            assert.strictEqual( typeof Scriptor.addYieldHandler, 'function', 'No addYieldHandler function exported' );
         } );
     } );
 };
