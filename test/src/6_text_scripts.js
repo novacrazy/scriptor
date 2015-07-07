e55c71fa22db6e3ecb98f2a3be6db83656ba891f
novacrazy <novacrazy@gmail.com>
2015-07-07 06:52:14 -0500

    test: Add tests for TextScript

---
+++ test/src/6_text_scripts.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Aaron on 7/7/2015.
+ */
+
+import {runTests} from './runner.js';
+
+import assert from 'assert';
+
+let tests = ( Scriptor, build ) => {
+    describe( `TextScripts (${build} build)`, () => {
+        let TextScript = Scriptor.TextScript;
+
+        describe( 'simple file with hello world', function() {
+            let script = new TextScript( './test/fixtures/text/hello.txt', module );
+
+            it( 'should not export anything', function( done ) {
+                script.exports().then( function( script_exports ) {
+                    assert.deepEqual( script_exports, {} );
+                    assert( script.loaded );
+
+                } ).then( done );
+            } );
+
+            it( 'should give the text as the script source', function( done ) {
+                script.source( 'utf-8' ).then( src => {
+                    assert.strictEqual( src.trim(), 'Hello, World!' );
+
+                } ).then( done );
+            } );
+        } );
+    } );
+};
+
+runTests( 'compat', tests );
+runTests( 'modern', tests );
