a5301cacf4aa92eea647f10c8edb2b27290b1a71
novacrazy <novacrazy@gmail.com>
2015-07-08 08:19:49 -0500

    test: Add a few initial tests for the Manager.

---
+++ test/src/8_manager.js
@@ -0,0 +1,51 @@
+/**
+ * Created by Aaron on 7/7/2015.
+ */
+
+import {runTests} from './runner.js';
+
+import Module from 'module';
+import {resolve} from 'path';
+
+import assert from 'assert';
+
+let tests = ( Scriptor, build ) => {
+    describe( `Managers (${build} build)`, function() {
+        let {Manager, Script} = Scriptor;
+
+        let manager;
+
+        describe( 'Creating a manager', function() {
+            manager = new Manager( module );
+
+            it( 'should result in an empty manager with a parent module', function() {
+                assert( manager.parent instanceof Module );
+                assert.strictEqual( manager.parent.parent, module );
+                assert( manager.scripts instanceof Map );
+                assert.strictEqual( manager.scripts.size, 0 );
+            } );
+
+            it( 'should set cwd to process.cwd', function() {
+                assert.strictEqual( manager.cwd(), process.cwd() );
+            } );
+
+            it( 'should allow changing cwd', function() {
+                manager.chdir( './fixtures' );
+
+                assert.strictEqual( manager.cwd(), resolve( process.cwd(), './fixtures' ) );
+            } );
+        } );
+
+        describe( 'Add simple scripts through a manager instance', function() {
+            it( 'should return the Script instance when passed a path relative to the cwd', function() {
+                let script = manager.add( 'empty.js' );
+
+                assert( script instanceof Script );
+                assert.strictEqual( script.filename, resolve( process.cwd(), './fixtures/empty.js' ) );
+            } );
+        } );
+    } );
+};
+
+runTests( 'compat', tests );
+runTests( 'modern', tests );
